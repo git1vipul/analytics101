3bd9f2301c4ab413ddd94a0115c7a2cd18256d46
VIpul <vipulanand>
2018-03-29 18:46:50 +0530

    Modified

--- ds01.R
+++ ds01.R
@@ -36,3 +36,52 @@ summary(df)
 
 ls()
 #rm(list=ls())
+(mylist1 = list(1,df, v4))
+?list
+?list()
+mylist1
+(mymatrix = matrix(1:24,nrow = 6, byrow = T))
+?matrix
+class(mymatrix)
+mymatrix
+mymatrix[2,3:4]
+mymatrix[2:3,3:4]
+mymatrix[2,1]
+dimnames(mymatrix) = list(c('R1','R2','R3','R4','R5','R6'),
+                          c(paste('c',1:4,sep='')))
+mymatrix
+dimnames(mymatrix) = list(c(paste('R',1:6,sep = '_')),c(paste('C',1:4,sep = '_')))
+mymatrix
+?array
+(myarray = array(1:24, dim = c(4,3,2)))
+dimnames(myarray) = list(c(paste('A', 1:4, sep = '_')),
+                         c(paste('A', 1:3, sep = '_')),
+                         c(paste('A', 1:2, sep = '_')))
+myarray
+fix(df)
+df 
+df$cource
+df[1,]
+df$gender = c('M','F','M','F')
+fix(df)
+str(df)
+?str
+df$gender = factor(df$gender)
+str(df)
+fix(df)
+df$grades = c('A', 'B', 'E', 'F')
+
+fix(df)
+str(df)
+df
+table(df$cource,df$gender)
+apply(mymatrix,1,sum)
+mymatrix
+apply(myarray,3,sum)
+apply(mymatrix,1,mean)
+apply(mymatrix,2,mean)
+apply(mymatrix,2,sd)
+summary()
+tapply(df$marks, df$cource, sum)
+tapply(df$marks, df$gender, sum)
+tapply(df$marks, df$gender, mean)
