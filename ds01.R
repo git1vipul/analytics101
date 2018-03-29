f0d07418842d706140001a3fe871d142b5d5733a
VIpul <vipulanand>
2018-03-29 14:39:34 +0530

    Vectors

---
+++ ds01.R
@@ -0,0 +1,23 @@
+#Data Structure in R
+
+#Vectors----
+(v1 = c('A','B','C'))
+(v2= c(1,2,3))
+(v3 = c(TRUE,FALSE,TRUE,FALSE,FALSE))
+class(v3)
+v4 = 100:200
+v4
+v3a = c(T,F,T)
+v3a
+v4[-c(1,3,4)]
+v1[c(2)]
+v4[v4==151]
+v7=c('A','B')
+v7[1]
+(v8 = 40:45)
+names(v8) = c('vipul', 'hekko', 'lorem', 'ipsum', 'helo')
+v8[5]
+name(lorem)
+v8['hekko']
+v8[2]
+v8[c('hekko', 'helo')]
